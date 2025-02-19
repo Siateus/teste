a2101274c088c0c57d039e0300a0b6162ea52bbb
Siateus <tecmathe@gmail.com>
2025-02-19 07:55:04 -0300

    update

--- components/Telas/Index.js
+++
@@ -1,7 +0,0 @@
-
-export { default as TelaFuncionario } from './TelaFuncionario';
-export { default as TelaLogin } from './TelaLogin';
-export { default as TelaMensagem } from './TelaMensagem';
-export { default as TelaNotificacao } from './TelaNotificacao';
-export { default as TelaPerfil } from './TelaPerfil';
-
